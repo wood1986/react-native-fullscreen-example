c3349d16b701b31c292067cf86a72a4b3b08f5f2
wood1986 <5212215+wood1986@users.noreply.github.com>
2022-11-01 13:29:11 -0700

    deinit message

--- App.tsx
+++ App.tsx
@@ -8,25 +8,37 @@
 //  * @format
 //  */
 
-import React, {useRef} from 'react';
-import {
-  View
-} from 'react-native';
+import React, {useRef, useState} from 'react';
+import {SafeAreaView, Button} from 'react-native';
 import Video from 'react-native-video';
 
 export default function () {
   const videoRef = useRef<Video>(null);
-  return <View style={{height: '50%', width: '50%', backgroundColor: 'blue'}}>
-    <Video
-      ref={videoRef}
-      source={require('./playstation.mp4')}
-      controls={true}
-      resizeMode={'contain'}
-      paused={true}
-      style={[{flex: 1, height: '100%', width: '100%'}]}
-      onLoad={() => {
-        videoRef.current?.seek(0);
-      }}
-    />
-  </View>
+  const [on, setOn] = useState(true);
+
+  return (
+    <SafeAreaView style={{height: '50%', width: '50%', backgroundColor: 'blue'}}>
+      <Button
+        title={'Toggle'}
+        onPress={() => {
+          setOn(!on);
+        }}
+      />
+      {on ? (
+        <Video
+          ref={videoRef}
+          source={require('./playstation.mp4')}
+          controls={true}
+          resizeMode={'contain'}
+          paused={true}
+          style={[{flex: 1, height: '100%', width: '100%'}]}
+          onLoad={() => {
+            videoRef.current?.seek(0);
+          }}
+        />
+      ) : (
+        <></>
+      )}
+    </SafeAreaView>
+  );
 }
