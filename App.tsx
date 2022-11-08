65d7c2221df786bd94f5d703ced414507773aec1
wood1986 <5212215+wood1986@users.noreply.github.com>
2022-11-08 00:50:22 -0800

    WIP

--- App.tsx
+++ App.tsx
@@ -8,25 +8,33 @@
 //  * @format
 //  */
 
-import React, {useRef} from 'react';
-import {
-  View
-} from 'react-native';
+import React, {useRef, useState} from 'react';
+import {Button, SafeAreaView} from 'react-native';
 import Video from 'react-native-video';
 
 export default function () {
+  const videos = [require('./playstation1.mp4'), require('./playstation2.mp4')];
+  const [index, setIndex] = useState(0);
+
   const videoRef = useRef<Video>(null);
-  return <View style={{height: '50%', width: '50%', backgroundColor: 'blue'}}>
-    <Video
-      ref={videoRef}
-      source={require('./playstation.mp4')}
-      controls={true}
-      resizeMode={'contain'}
-      paused={true}
-      style={[{flex: 1, height: '100%', width: '100%'}]}
-      onLoad={() => {
-        videoRef.current?.seek(0);
-      }}
-    />
-  </View>
+  return (
+    <SafeAreaView style={{height: '50%', width: '50%', backgroundColor: 'blue'}}>
+      <Button
+        title="NEXT"
+        onPress={() => {
+          setIndex((index + 1) % videos.length);
+        }}
+      />
+      <Video
+        ref={videoRef}
+        source={videos[index]}
+        controls={true}
+        resizeMode={'contain'}
+        style={[{flex: 1, height: '100%', width: '100%'}]}
+        onLoad={() => {
+          videoRef.current?.seek(0);
+        }}
+      />
+    </SafeAreaView>
+  );
 }
